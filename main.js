1d497608dd4bcdda1cde0846dba00f688b916cc8
Trevor Reimer <trevorreimer95@gmail.com>
2018-11-04 14:53:14 -0500

    added the share feature

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 let textarea;
 let p;
 
+window.addEventListener("hashchange", function(){
+    if(location.hash && location.hash.length > 1){
+        textarea.value = location.hash.replace(/#|-/g, " ").trim();
+    }
+
+    parse();
+});
+
+
 function setup(){
     createCanvas(500, 500);
     angleMode(DEGREES);
@@ -10,6 +19,10 @@ function setup(){
         parse();
     });
 
+    if(location.hash && location.hash.length > 1){
+        textarea.value = location.hash.replace(/#|-/g, " ").trim();
+    }
+
     parse();
 }
 
@@ -17,4 +30,9 @@ function parse(){
     background(0);
 
     p = new Parser(textarea.value.trim());
+}
+
+function share(){
+    location.hash = textarea.value.trim().replace(/\s/g, "-");
+    // console.log(textarea.value.trim().replace(/\s/g, "-"));
 }
